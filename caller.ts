a3482c3e5fd967bac9d283490833978632e97d70
Alexandre Piel <alexandre.piel@gmail.com>
2020-05-31 19:33:16 +0200

    caller levelUp

--- caller.ts
+++ caller.ts
@@ -1,10 +1,6 @@
-export const defaultLevelUp = 3;
+export const base = 3;
 
-export function callerUp(level: number) {
-    return caller(defaultLevelUp + level);
-}
-
-export default function caller(levelUp = defaultLevelUp) {
+export default function caller(levelUp = base) {
     const err = new Error();
     const stack = err.stack?.split('\n')[levelUp];
     if (stack) {
