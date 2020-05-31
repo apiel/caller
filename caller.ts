e16ddaadc86df2dd3b93a909e9c00fca4aa8efe4
Alexandre Piel <alexandre.piel@gmail.com>
2020-05-31 19:16:59 +0200

    level up

--- caller.ts
+++ caller.ts
@@ -1,6 +1,12 @@
-export default function caller() {
+export const defaultLevelUp = 3;
+
+export function callerUp(level: number) {
+    caller(defaultLevelUp + level);
+}
+
+export default function caller(levelUp = defaultLevelUp) {
     const err = new Error();
-    const stack = err.stack?.split('\n')[3];
+    const stack = err.stack?.split('\n')[levelUp];
     if (stack) {
         const path = stack.substr(stack.indexOf('(')+1).split(':');
         const file = `${path[0]}:${path[1]}`;
