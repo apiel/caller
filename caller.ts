aac46260bca8c89cedf242da1cadcd51e32f0326
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-02 20:45:14 +0200

    fix annonymous call

--- caller.ts
+++ caller.ts
@@ -4,8 +4,34 @@ export default function caller(levelUp = up) {
     const err = new Error();
     const stack = err.stack?.split('\n')[levelUp];
     if (stack) {
-        const path = stack.substr(stack.indexOf('(')+1).split(':');
-        const file = `${path[0]}:${path[1]}`;
-        return file;
+        return getFile(stack);
     }
+}
+
+function getFile(stack: string) {
+    stack = stack.substr(stack.indexOf('at ') + 3);
+    if (!stack.startsWith('file://')) {
+        stack = stack.substr(stack.indexOf('(') + 1);
+    }
+    const path = stack.split(':');
+    const file = `${path[0]}:${path[1]}`;
+    return file;
+}
+
+if (import.meta.main) {
+    console.log(
+        getFile(
+            '     at Object.Item [as component] (file:///home/alex/demo/pages/item/[id].page.tsx:13:13)',
+        ),
+    );
+    console.log(
+        getFile(
+            '     at file:///home/alex/demo/pages/Product/[color]/[id].page.tsx:20:16',
+        ),
+    );
+    console.log(
+        getFile(
+            '     at file:///home/alex/demo/pages/Product/(color)/[id].page.tsx:20:16',
+        ),
+    );
 }
